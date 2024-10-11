d5490fdd70b70d629dd326203bb3c3ab109f4dfa
dhruvordangi <146311496+dhruvordangi@users.noreply.github.com>
2024-10-11 12:38:12 +0000

    all

--- 01basicreact/01vitereact/src/App.jsx
+++ 01basicreact/01vitereact/src/App.jsx
@@ -1,7 +1,11 @@
-
+import Chai from "./chai";
 function App() {
   return (
-    <h1>CHai aur react | DD</h1>
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
   );
 }
 export default App
