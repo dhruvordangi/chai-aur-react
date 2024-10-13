5f8cc7013cd0cd2f24cb0a5e9942a7ffd578468a
dhruvordangi <146311496+dhruvordangi@users.noreply.github.com>
2024-10-13 07:24:23 +0000

    all

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,15 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let myObject={
+    name:"dhrv",
+    age:21
+  }  
+  let newArr=[1,2,4]
 
   return (
     <>
-      <h1 className='bg-green-'>Tailwind test</h1>
+      <h1 className='bg-green-400 text-blue p-4 rounded-xl' >Tailwind test</h1>
+      <Card channel="chai aur code" someobj={myObject} btnText="clickMe" arr={newArr} username="dangis" />
+      <Card username="dhruv dangi" btnText="visitMe" />
     </>
   )
+  
 }
 
 export default App
