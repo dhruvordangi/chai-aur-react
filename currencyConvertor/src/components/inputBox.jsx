52c43705be5dedf4e9f2e1a7e52b8d161c4e92f1
dhruvordangi <146311496+dhruvordangi@users.noreply.github.com>
2024-10-15 14:53:01 +0000

    all

--- currencyConvertor/src/components/inputBox.jsx
+++ currencyConvertor/src/components/inputBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useId} from 'react'
 
 
 function InputBox({
@@ -6,23 +6,22 @@ function InputBox({
     amount,
     onAmountChange,
     onCurrencyChange,
-    currencyOption=[],
+    currencyOptions=[],
     selectCurrency="usd",
-    amountDisable=false,
     amountDisable=false,
     currencyDisable=false,
     className = "",
 }) {
-   
+    const amountInputId= useId()
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
-                    label
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
+                    {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}  //binding donw with the input
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -35,12 +34,15 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+
                 >
-                    
-                        <option value="usd">
-                            "usd"
-                        </option>
+                    {currencyOptions.map( (currency) => (
+                        <option key={currency} value={currency}>{currency}</option>
+                    ) )}
+                        
                 
                 </select>
             </div>
--- currencyConvertor/src/useCurrencyinfo.js
+++ currencyConvertor/src/useCurrencyinfo.js
@@ -5,7 +5,7 @@ function useCurrencyInfo(currency){
     useEffect( () =>{
         fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
         .then( (res) => res.json() )
-        .then( (res) =>setData(res[currency]) )
+        .then( (res) => setData(res[currency]) )
         console.log(data);
         
     }, [currency] )
