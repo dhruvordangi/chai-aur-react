65de12da1905e8ca6508e3e29610f7988f93bf3e
dhruvordangi <146311496+dhruvordangi@users.noreply.github.com>
2024-10-15 11:24:04 +0000

    all

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'>Currerncy App</h1>
+    </>
+  )
+}
+
+export default App
