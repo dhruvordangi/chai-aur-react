fe89c4ef00cf08e5bf53959d1ae48df64dea2021
dhruvordangi <dhruvdangi03@gmail.com>
2024-10-19 09:16:40 +0530

    all

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 import './App.css'
+import Header from './components/header/header'
+import Home from './components/Home/home'
+import Footer from './components/footer/footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-400 p-4 '>React Router</h1>
     </>
   )
 }
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+
+import Layout from './layout.jsx';
+import Home from './components/Home/home.jsx'
+import About from './components/About/About.jsx'
+import{ RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+const router= createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
